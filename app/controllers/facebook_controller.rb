549875bc60a5b8d75cd9873e66052fc5f70b8f25
PK Banks <pkbanks@meme.menu>
2014-06-10 19:54:27 -0400

    integrated the search and results into a single 'index' view via wrapping the code in an if-block, see the controller

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,14 +1,13 @@
 class FacebookController < ApplicationController
 
 	def index
-  	# @search_for = "jamiefoxx"
-
+		if params[:name]
+	  	@search_for = params[:name]
+	  	response = HTTParty.get("http://graph.facebook.com/#{@search_for}")
+	  	@profile = JSON::parse(response.body)
+  	end
   end
 
   def show
-  	@search_for = params[:name]
-  	# @search_for = "jamiefoxx"
-  	response = HTTParty.get("http://graph.facebook.com/#{@search_for}")
-  	@profile = JSON::parse(response.body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   root 'facebook#index'
 
-  post 'facebook/show', to: 'facebook#show'
+  # post 'facebook/show', to: 'facebook#show'
+  post 'facebook/index', to: 'facebook#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
