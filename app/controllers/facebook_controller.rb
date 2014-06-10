24763cecf01d3dcd20edde1ab23939355b24a5f5
PK Banks <pkbanks@meme.menu>
2014-06-10 16:08:36 -0400

    Remapped root page to 'facebook#index' and updated view to display the key:value pairs for a @profile.

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,4 +1,6 @@
 class FacebookController < ApplicationController
   def index
+  	response = HTTParty.get('http://graph.facebook.com/pkbanks12')
+  	@profile = JSON::parse(response.body)
   end
 end
