9564d404f42765cb5d79f68d0edc527c09e4b16f
PK Banks <pkbanks@meme.menu>
2014-06-10 17:54:54 -0400

    new view 'show', changed 'index' to search form.  now we can search and see the result

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,6 +1,14 @@
 class FacebookController < ApplicationController
-  def index
-  	response = HTTParty.get('http://graph.facebook.com/pkbanks12')
+
+	def index
+  	# @search_for = "jamiefoxx"
+
+  end
+
+  def show
+  	@search_for = params[:name]
+  	# @search_for = "jamiefoxx"
+  	response = HTTParty.get("http://graph.facebook.com/#{@search_for}")
   	@profile = JSON::parse(response.body)
   end
 end
