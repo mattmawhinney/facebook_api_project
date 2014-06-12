b2dd1eee86def8e1541b7b8f5cd1dc6fc106ce7f
PK Banks <pkbanks@meme.menu>
2014-06-12 16:17:27 -0400

    handles when there is white space in the search field, controller removes whitespace via regex

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -2,7 +2,7 @@ class FacebookController < ApplicationController
 
 	def index
 		if params[:name]
-			@search_for = params[:name]
+			@search_for = params[:name].gsub(/\s+/, "")	# remove all whitespace, because we are searching for fb usernames
 			response = HTTParty.get("http://graph.facebook.com/#{@search_for}")
 			@profile = JSON::parse(response.body)
 		end
